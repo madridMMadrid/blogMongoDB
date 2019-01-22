a39a45d936643028768c6ae1513328c3c0e13e3b
madridMMadrid <daffidak@bk.ru>
2019-01-22 16:14:46 +0300

    work learn 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const Post = require('./models/post');
 
@@ -7,7 +8,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+    // body-parser извлекает всю часть тела входящего потока запросов и предоставляет его на req.body
+    // таким образом мы можем достовать из html атрибута name то что нам необходимо
+app.use(express.static(path.join(__dirname, 'public')));
+    // указываем express статичный файл который нужно выдавать пользователю а именно папку public
+app.use(
+    '/script', // указываем конкретно что отдать
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+        // указываем конкретный путь, в данном случае к jquery
+); 
 
 const arr = [
     'hello',
