64f14bcb13017174ae94a06294abf4b66c7f48a7
madridMMadrid <daffidak@bk.ru>
2019-01-22 16:30:34 +0300

    вторая часть work

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const Post = require('./models/post');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,33 +16,11 @@ app.use(
         // указываем конкретный путь, в данном случае к jquery
 ); 
 
-const arr = [
-    'hello',
-    'world',
-    'test'
-];
 
 app.get('/', (req, res) => {
-    Post.find({}).then(posts => {
-        // обращаемся к базе данных по сущьности и находим в ней весь обьект
-        res.render('index', {posts: posts});
-            // рендерим в index то что находится в сущьности
-    })
+        res.render('index');
 });
 
 
-// res.render('index', {arr: arr}));
-
-app.get('/create', (req, res) => res.render('create'));
-app.post('/create', (req, res) => {
-    const {title, body} = req.body;
-    // arr.push(req.body.text);
-    Post.create({
-        title: title,
-        body: body
-    }).then(post => console.log(post.id))
-
-    res.redirect('/');
-});
 
 module.exports = app;
