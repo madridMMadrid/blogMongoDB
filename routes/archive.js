c5a121e08d18a22c29e029e4e01894d0563c83fd
madridMMadrid <daffidak@bk.ru>
2019-01-24 19:19:19 +0300

    ссылки на отдельные посту и их реализация

--- routes/archive.js
+++ routes/archive.js
@@ -26,9 +26,13 @@ function posts(req, res) {
             }
           });
         })
-        .catch(console.log);
+        .catch(() => {
+					throw new Error('Server error')
+				});
     })
-    .catch(console.log);
+    .catch(() => {
+			throw new Error('Server error')
+		});
 }
 
 // routers
@@ -36,5 +40,34 @@ function posts(req, res) {
 router.get('/', (req, res) => posts(req, res));
 // при переходе на href="/archive/<%= Number(current) - 1 %>" у нас будет вызыватся тажа функция с нужным нам контентом
 router.get('/archive/:page', (req, res) => posts(req, res));
+router.get('/posts/:post', (req, res, next) => {
+    const url = req.params.post.trim().replace(/ +(?= )/g, '');
+    const userId = req.session.userId;
+    const userLogin = req.session.userLogin;
+
+    if (!url) {
+        const err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    } else {
+        models.Post.findOne({
+            url
+        }).then(post => {
+            if (!post) {
+                const err = new Error('Not Found');
+                err.status = 404;
+                next(err);
+            } else {
+							res.render('post/post', {
+								post,
+								user: {
+									id: userId,
+									login: userLogin
+								}
+							});
+						}
+        })
+    }
+});
 
 module.exports = router;
